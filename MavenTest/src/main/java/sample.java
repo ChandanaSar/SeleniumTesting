996b2a9552842089569f0f18f1c020577185f538
ChandanaSar <59591265+ChandanaSar@users.noreply.github.com>
2020-02-19 16:34:34 -0800

    Maven Test Added

---
+++ MavenTest/src/main/java/sample.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+				 
+				WebDriver driver = new FirefoxDriver();
+				//driver.get("http://www.google.com");
+				
+				String baseUrl = "http://demo.guru99.com/test/newtours/";
+		        String expectedTitle = "Welcome: Mercury Tours";
+		        String actualTitle = "";
+
+		        // launch Fire fox and direct it to the Base URL
+		        driver.get(baseUrl);
+
+		        // get the actual value of the title
+		        actualTitle = driver.getTitle();
+
+		        /*
+		         * compare the actual title of the page with the expected one and print
+		         * the result as "Passed" or "Failed"
+		         */
+		        if (actualTitle.contentEquals(expectedTitle)){
+		            System.out.println("Test Passed!");
+		        } else {
+		            System.out.println("Test Failed");
+		        }
+		       
+		        //close Fire fox
+		        driver.close();
+
+	}
+
+}
